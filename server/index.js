08707ad17afc4234fe12a100380e46456cdfddff
gxr21 <alijalal200311@gmail.com>
2026-07-20 10:16:41 +0300

    update server

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ const transporter = nodemailer.createTransport({
   port: Number(process.env.SMTP_PORT) || 587,
   secure: Number(process.env.SMTP_PORT) === 465,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
+  family: 4,
 })
 
 function escapeHtml(value) {
